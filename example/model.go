a92534f3108fac236541fb431334d21a236a3db5
laqiiz <mano.junki@gmail.com>
2019-08-18 20:41:24 +0900

    add Test

--- example/model.go
+++ example/model.go
@@ -1,4 +1,4 @@
-//go:generate juv $GOFILE
+//go:generate juv -o ./model_gen.go $GOFILE
 package example
 
 import (
@@ -6,32 +6,9 @@ import (
 )
 
 type Post struct {
-	ID      int       `validate:"gt=0" json:"id"`
-	Created time.Time `validate:"max=11" json:"created"`
-	Draft   bool      `json:"draft"`
-	Title   string    `validate:"required"`
-	Body    string    `validate:"max=50"`
+	ID      int       `validate:"gt=0"     json:"id"`
+	Created time.Time `validate:"max=11"   json:"created"`
+	Title   string    `validate:"required" json:"title"`
+	Body    string    `validate:"max=50"   json:"body"`
+	Draft   *bool     `validate:"required" json:"draft"`
 }
-
-//func (r *Post) UnmarshalJSON(b []byte) error {
-//	// ------------ ここからは定形 ---------------
-//	type Pointer Post // Avoid stack over flow
-//	var p Pointer
-//
-//	if err := json.Unmarshal(b, &p); err != nil {
-//		return err
-//	}
-//	// ------------ 定形 -------------------------
-//
-//	if err := validator.New().Struct(p); err != nil {
-//		return err
-//	}
-//
-//	// レシーバー変数に代入する処理
-//	r.ID = p.ID
-//	r.Created = p.Created
-//	r.Draft = p.Draft
-//	r.Body = p.Body
-//
-//	return nil
-//}
